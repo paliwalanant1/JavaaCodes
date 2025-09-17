b94c3fbce1a8aef65d4955fb5cfe54d768d72e72
paliwalanant1 <paliwalanant1@gmail.com>
2025-09-18 00:11:41 +0530

    Add files via upload

---
+++ PracticeOne/Calculating.java
@@ -0,0 +1,8 @@
+package PracticeOne;
+
+public class Calculating {
+    public static void main(String[] args) {
+        Rectangle obj = new Rectangle(10, 20);
+        obj.area();
+    }
+}
---
+++ PracticeOne/Rectangle.java
@@ -0,0 +1,16 @@
+package PracticeOne;
+//import java.util.Scanner;
+public class Rectangle {
+    public int length;
+    public int width;
+
+    public Rectangle(int length, int width){
+        this.length = length;
+        this.width = width;
+    }
+
+    public void area(){
+        System.out.println("Area: " + (length*width));
+        System.out.println("Perimeter: " + 2*(length+width));
+    }
+}
